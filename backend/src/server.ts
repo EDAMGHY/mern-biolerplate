a1d37d138c4a224e42a19a90d1b3810e2f2a8149
Abdellah DAMRI <damriabdellah@gmail.com>
2023-12-12 15:39:54 +0100

    Add: adding the mongodb setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import connectDB from "@/config/db";
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
